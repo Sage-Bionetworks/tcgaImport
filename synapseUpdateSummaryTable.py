c13be444fc5df8f77ad83fc77a5d23d259641c67
lomberg <lom@larssono.com>
2015-04-24 16:26:25 -0700

    Make the table delete functionality a batch operation.

--- synapseUpdateSummaryTable.py
+++ synapseUpdateSummaryTable.py
@@ -12,6 +12,38 @@ TABLEID='syn3281840' #Original
 syn = synapseclient.login()
 
 
+def findUpdates(files, tableId=TABLEID):
+    """Compares the lastmodified date in the table and file anntoations.
+    
+    files is a table of file medata including the column lastModified.
+    """
+    table = syn.tableQuery('SELECT distinct id, versionNumber FROM %s' %tableId)
+    df = table.asDataFrame().set_index('id')
+    toUpdate=[]
+    for i, f in files.iterrows():
+        if f.id not in df.index:
+            toUpdate.append(i)
+        else:
+            if df.ix[f.id,'versionNumber'] < f.versionNumber:
+                toUpdate.append(i)
+    return files.ix[toUpdate,:]
+
+
+def deleteAffectedRows(updatedFiles, tableId=TABLEID):
+    """Given a list of file Entities.  Extracts all rows in the table
+    that match the id column in the updatedFiles data frame and
+    deletes them from Synapse."""
+
+    #Build query to find rows to delete for update
+    idSet = "('" + "','".join(updatedFiles.id) +"')"
+    queryStr = "select id from %s where id in %s" %(tableId, idSet)
+    #Query and delete
+    rowSet = syn.tableQuery(queryStr).asRowSet()
+    if len(rowSet['rows'])>0:
+        print 'DELETING', len(rowSet['rows']), 'ROWS'
+        syn.delete(rowSet)
+
+
 def countAndUpdateTable(input, path='out/', tableId=TABLEID):
     i, fileMeta = input
     if fileMeta.fileType=='ttl':
@@ -33,38 +65,16 @@ def countAndUpdateTable(input, path='out/', tableId=TABLEID):
     metadata.drop(['projectId', 'tissue', u'md5', u'assembly'], axis=1, inplace=True)
     metadata.nFeatures = metadata.nFeatures.astype('int')
 
-    #Remove existing rows in table
-    queryStr = "select id from %s where id='%s'" %(tableId, fileMeta.id)
-    #rowSet = syn.tableQuery(queryStr, resultsAs="rowset", isConsistent=False).asRowSet()
-    rowSet = syn.tableQuery(queryStr).asRowSet()
-    if len(rowSet['rows'])>0:
-        print 'deleting', len(rowSet['rows'])
-        syn.delete(rowSet)
     #Update rows in table
     print 'adding', metadata.shape[0]
     t = syn.store(Table(tableId, metadata))
     return metadata
 
 
-def findUpdates(files, tableId=TABLEID):
-    """Compares the lastmodified date in the table and file anntoations.
-    
-    files is a table of file medata including the column lastModified.
-    """
-    table = syn.tableQuery('SELECT distinct id, versionNumber FROM %s' %tableId)
-    df = table.asDataFrame().set_index('id')
-    toUpdate=[]
-    for i, f in files.iterrows():
-        if f.id not in df.index:
-            toUpdate.append(i)
-        else:
-            if df.ix[f.id,'versionNumber'] < f.versionNumber:
-                toUpdate.append(i)
-    return files.ix[toUpdate,:]
-
 
 if __name__ == '__main__':
     files = synapseHelpers.query2df(syn.chunkedQuery(FILEQUERY), savedSynapseFields=('id', 'name', 'versionNumber'))
     updatedFiles= findUpdates(files)
-    print 'NEED TO UPDATE:', updatedFiles.shape[0]
-    #dfs = map(countAndUpdateTable, updatedFiles.iterrows())
+    print 'NEED TO UPDATE:', updatedFiles.shape[0], 'FILES'
+    deleteAffectedRows(updatedFiles)
+    dfs = map(countAndUpdateTable, updatedFiles.iterrows())
