85514b19e121edd73b48b0c4ad4d3d782b07fde2
duncan-palmer <duncan.palmer@sagebase.org>
2015-11-25 12:38:40 -0800

    Updated README, fixed mergeByPlatform.py and ran through code to make sure it works.

--- mergeByPlatform.py
+++ mergeByPlatform.py
@@ -29,8 +29,8 @@ availPlatforms = '\n'.join(set([(x[0]+'\t') for x in platforms]))
 parser = argparse.ArgumentParser()
 parser.add_argument('benefactorId',help='ID of synapse project to merge files from.')
 parser.add_argument('parentId',help='ID of synapse project to add merged file to.')
-parser.add_argument('filePath',default='.',help='Local filepath to write merged files to. Defaults to current directory.',type=str)
-parser.add_argument('-p','--platforms',nargs='*',help='If merging subset of platform type, add platform(s) name after option. \
+parser.add_argument('-f','--filepath',default='.',help='Local filepath to write merged files to. Defaults to current directory.',type=str)
+parser.add_argument('-p','--platforms',nargs='*',help='If merging subset of platform type, add platform(s) name after option, separated by whitespace. \
 		    Available platforms' + '\n' +  availPlatforms,type=str)
 args = parser.parse_args()
 if args.platforms is not None:
@@ -38,8 +38,9 @@ if args.platforms is not None:
 query_str = "select * from file where benefactorId==" + ("'{0}'").format(args.benefactorId)
 
 
-def isUptodate(name, files,parentId):
-    if not syn._findEntityIdByNameAndParent(name,parentId):
+def isUptodate(name, files, parentId):
+    id = syn._findEntityIdByNameAndParent(name,parentId)
+    if id is None:
         return False
     activity = syn.getProvenance(id)
     used = set(['%s.%s' % (x['reference']['targetId'], x['reference']['targetVersionNumber']) for x in activity['used'] if x['wasExecuted']==False])
@@ -60,17 +61,17 @@ for platform, dataSubType, name in platforms:
     if list(set(filteredMeta.fileType))[0] in ['seg','bed']:
         dfs = mp.map(lambda f: pd.read_csv(f.path, sep='\t'), files)
         df = pd.concat(dfs, axis=0)
-        df.to_csv(args.filePath+name, sep='\t', index=False)
+        df.to_csv(args.filepath+name, sep='\t', index=False)
         nSamples = len(set(df.Sample))
         nFeatures = 0
     else: #All other fileTypes
         dfs = mp.map(lambda f: pd.read_csv(f.path, sep='\t', index_col=0), files)
         df = pd.concat(dfs, axis=1)
-        df.to_csv(args.filePath+name, sep='\t')
+        df.to_csv(args.filepath+name, sep='\t')
         nFeatures, nSamples = df.shape
     print 'Created', name, df.shape
     #Add file to Synapse
-    entity = synapseclient.File(args.filePath+name, parentId=args.parentId)
+    entity = synapseclient.File(args.filepath+name, parentId=args.parentId)
     #Set annotations
     entity.platform = platform
     entity.dataSubType = dataSubType
@@ -83,4 +84,4 @@ for platform, dataSubType, name in platforms:
     entity.platformTitle = list(set(filteredMeta.platformTitle))
     entity.nSamples = nSamples 
     entity.nFeatures = nFeatures 
-    entity = syn.store(entity, used = files, executed=thisCodeInSynapse(parentId='syn1774100'))
+    entity = syn.store(entity, used=files, executed=thisCodeInSynapse(parentId=args.parentId))
